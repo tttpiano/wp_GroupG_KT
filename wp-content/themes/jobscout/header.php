4ea38534c1d83538d70b452a327e7a897b57cdc0
nguyencongmen <nguyenme123321@gmail.com>
2023-11-21 21:38:33 +0700

    UPDATE

--- wp-content/themes/jobscout/header.php
+++ wp-content/themes/jobscout/header.php
@@ -25,6 +25,7 @@
     do_action( 'jobscout_before_wp_head' );
     
     wp_head(); ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body <?php body_class(); ?> itemscope itemtype="https://schema.org/WebPage">
