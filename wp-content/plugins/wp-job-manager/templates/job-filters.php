0e5d3d798582ef6f76f24107f0118827c01cdf6d
VietThang <21211tt3051@mail.tdc.edu.vn>
2023-11-22 01:06:56 +0700

    update Contact

--- wp-content/plugins/wp-job-manager/templates/job-filters.php
+++ wp-content/plugins/wp-job-manager/templates/job-filters.php
@@ -19,7 +19,7 @@ wp_enqueue_script( 'wp-job-manager-ajax-filters' );
 
 do_action( 'job_manager_job_filters_before', $atts );
 ?>
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <form class="job_filters">
 	<?php do_action( 'job_manager_job_filters_start', $atts ); ?>
 
